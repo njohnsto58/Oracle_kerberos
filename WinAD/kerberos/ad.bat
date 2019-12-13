43bbf9d66b98e1f931f63ba4611558165c9a18d1
njohnsto <njohnsto58@gmail.com>
2019-12-13 17:57:10 +0000

    Update ad.bat

--- WinAD/kerberos/ad.bat
+++ WinAD/kerberos/ad.bat
@@ -5,6 +5,6 @@ set AD_NODE=win-2016
 set AD_DOMAIN=WINSN1.DBSEC.ORACLEVCN.COM
 set AD_DOMAIN_DN=dc=winsn1,dc=dbsec,dc=oraclevcn,dc=com
 set AD_CONTAINER=ou=Service Accounts,ou=Shared Infrastructure
-set DB_DOMAIN_REALMS=.publicsn1.dbsec.oraclevcn.com .dbsn1.dbsec.oraclevcn.com .dbsn2.dbsec.oraclevcn.com
+set DB_DOMAIN_REALMS=publicsn1.dbsec.oraclevcn.com dbsn1.dbsec.oraclevcn.com dbsn2.dbsec.oraclevcn.com
 set SCP_KEY="..\.ssh\frank_id_rsa.ppk"
 set AD_CERT_DIR=C:\Users\opc\Documents
